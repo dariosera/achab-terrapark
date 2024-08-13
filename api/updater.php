f89c5df6501e17ed0adb551f0d0a7f0cc64acb77
Dario Sera <dario@sera.im>
2024-08-13 09:50:50 +0200

    Anche in updater.php cambiato il nome del file config

--- api/updater.php
+++ api/updater.php
@@ -5,7 +5,7 @@ require_once 'vendor/autoload.php';
 if (strpos($_SERVER['HTTP_HOST'],'localhost') === 0) {
     $config_yaml = __DIR__.'/.config.local.yaml';
 } else {
-   $config_yaml = __DIR__.'/.config.yaml';
+   $config_yaml = __DIR__.'/.htkadroconfig';
 }
 
 $updater = new \DarioSera\KadroApiCore\Updater($config_yaml);
